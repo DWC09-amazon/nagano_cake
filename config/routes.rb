cea76f67de00aa9eab86b3542221aa2a7a42c83c
Ubuntu <ubuntu@ip-172-31-29-165.ap-northeast-1.compute.internal>
2025-10-16 10:22:44 +0000

    feat

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
   # Devise
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
