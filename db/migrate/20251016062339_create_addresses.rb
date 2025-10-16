de16b98eab1540dc5a733050697512efcdf0fd0f
otanishuhei <shu.86.kingdom82@gmail.com>
2025-10-16 16:43:46 +0900

    Update 20251016062339_create_addresses.rb
    外部キー　編集

--- db/migrate/20251016062339_create_addresses.rb
+++ db/migrate/20251016062339_create_addresses.rb
@@ -2,15 +2,12 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
       ## 会員id、宛名、郵便番号、住所
-      t.integer :customer_id, null: false
-      t.string  :name,        null: false
-      t.string  :potal_code,  null: false
-      t.string  :address,     null: false
+      t.references :customer,     null: false, foreign_key: true, type: :integer
+      t.string     :name,         null: false
+      t.string     :postal_code,  null: false
+      t.string     :address,      null: false
 
       t.timestamps null: false
     end
-
-    add_foreign_key :addresses, :customers, colum: :customer_id
-    add_index :addresses, :customer_id
   end
 end
