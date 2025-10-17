9212108770eb1473e8b6f00022b416b064e3daa1
Ubuntu <ubuntu@ip-172-31-29-165.ap-northeast-1.compute.internal>
2025-10-17 06:58:56 +0000

    admin-customer-model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,10 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_items, dependent: :destroy 
+  has_many :addresses, dependent: :destroy   
+  has_many :orders, dependent: :destroy
+
+  enum is_active: { active: 0, inactive: 1 }
 end
