c68feddd32d8ab4eb13babdbab0c0cbb6ed3ac8c
Ubuntu <ubuntu@ip-172-31-17-225.ap-northeast-1.compute.internal>
2025-10-22 08:47:48 +0000

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
   # Devise
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
